a8c570dc30c2d85df390a8ea142d1118eff37d04
Rafael Julio <development@rafifos.dev>
2026-06-16 10:17:11 -0300

    refactor: migrate airsync-macos to formula Switch from Homebrew Cask to Formula and add SHA256 verification
    Migrate from Homebrew Cask to Formula for airsync-mac

---
+++ Formula/airsync-mac.rb
@@ -0,0 +1,27 @@
+class AirSyncMacOS < Formula
+  desc "Bring the forbidden macOS continuity to Android"
+  homepage "https://sameerasw.com/airsync"
+  url "https://github.com/sameerasw/airsync-mac/archive/refs/tags/v3.2.0.tar.gz"
+  sha256 "3932935ea301b45db656ecf958cfbbb9223308c31ae7d8c14ba2e75e6b256e8b"
+  version "3.2.0"
+  license "MPL-2.0"
+
+  depends_on macos: :sonoma
+  depends_on xcode: ["14.5", :build]
+  depends_on brew: ["adb", "media-control", "scrcpy"]
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  def install
+    system "xcodebuild", "-scheme", "AirSync Self Compiled", "-configuration", "Release", "-derivedDataPath", "DerivedData"
+    app_path = buildpath/"DerivedData/Build/Products/Release/AirSync.app"
+    prefix.install app_path
+  end
+
+  test do
+    assert_predicate prefix/"AirSync.app", :exist?
+  end
+end
